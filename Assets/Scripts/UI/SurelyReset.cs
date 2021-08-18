84c48578e3da1e548e58630ce31ca6df903f999b
Your Name <you@example.com>
2021-08-18 12:36:28 +0700

    14

---
+++ Assets/Scripts/UI/SurelyReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurelyReset : MonoBehaviour
+{
+    public GameObject choosePanel;
+
+    public void Show()
+    {
+        choosePanel.SetActive(true);
+    }
+
+    public void Yes()
+    {
+        PlayerPrefs.DeleteAll();
+        Application.Quit();
+    }
+
+    public void No()
+    {
+        Loader.LoadScene(0);
+    }
+}
