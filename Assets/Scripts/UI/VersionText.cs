3bbbb159300e5accc08bd46e251f5c3bca7d844b
Your Name <you@example.com>
2021-08-21 14:16:36 +0700

    18

---
+++ Assets/Scripts/UI/VersionText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionText : MonoBehaviour
+{
+    private static string Version = "1.0";
+    private void Start()
+    {
+        GetComponent<Text>().text = Version;
+    }
+}
